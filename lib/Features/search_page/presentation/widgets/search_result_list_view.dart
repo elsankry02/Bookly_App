f314cec56ba59ebbb4337cc10cfc172b96504485
elsankry02 <githup800900@gmail.com>
2025-05-16 18:44:26 +0300

    fix: Correct import paths in search_result_list_view.dart and main.dart

--- lib/Features/search_page/presentation/widgets/search_result_list_view.dart
+++ lib/Features/search_page/presentation/widgets/search_result_list_view.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/core/utils/string_manger.dart';
+import '../../../../core/utils/string_manger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/material.dart';
+
 import 'app.dart';
 import 'core/services/services_locator.dart';
-import 'package:flutter/material.dart';
 
 void main() {
   setUpServicesLocator();
