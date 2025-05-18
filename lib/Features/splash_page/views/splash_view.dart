6921557ce6724e519c2550c467209e207aaa41e9
elsankry02 <githup800900@gmail.com>
2025-05-18 23:07:06 +0300

    feat: Refactor SplashView to use a single widget and remove SplashViewBody

--- lib/Features/splash_page/views/splash_view.dart
+++ lib/Features/splash_page/views/splash_view.dart
@@ -1,11 +1,61 @@
-import '../widgets/splash_view_body.dart';
+import 'package:bookly_app/Features/splash_page/widgets/slide_text.dart';
+import 'package:bookly_app/constant/image_manger.dart';
+import 'package:bookly_app/core/routes/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> with TickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SplashViewBody());
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(ImageManger.kBooklyLogo),
+        const SizedBox(height: 4),
+        SlideText(slidingAnimation: slidingAnimation),
+      ],
+    );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 4),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      GoRouter.of(context).pushReplacement(AppRouter.kHomeView);
+    });
   }
 }
