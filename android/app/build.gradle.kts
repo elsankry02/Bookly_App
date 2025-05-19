fcf3c4ef573fa21bb0d40357b1a53bbd20050093
elsankry02 <githup800900@gmail.com>
2025-05-19 11:22:57 +0300

    fix: Remove lintOptions from build.gradle.kts to streamline configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,10 +10,6 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
     
-    lintOptions {
-        warningsAsErrors false
-        checkReleaseBuilds false
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
